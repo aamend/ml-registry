8b8259097a04331bd07ce9963a3a45faaa861ade
Antoine Amend <antoine.amend@gmail.com>
2019-09-13 20:42:21 +0100

    new approach

--- README.md
+++
@@ -1,1 +0,0 @@
-# Spark ML Governance
--- src/main/scala/com/aamend/spark/gov/ml/package.scala
+++
@@ -1,8 +0,0 @@
-package com.aamend.spark.gov
-
-package object ml {
-  final val watermarkColumnParamName = "watermarkColumnParamName"
-  final val watermarkColumnParamDefault = "watermark"
-  final val watermarkParamName = "watermarkParamName"
-  final val watermarkParamDefault = "UNKNOWN"
-}
